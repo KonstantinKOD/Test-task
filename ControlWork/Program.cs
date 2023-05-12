389a87eaa43fb815e0dbf9e760728583f63a5ca5
Konsttantin <Ryabovkostya91@icloud.com>
2023-05-12 18:57:46 +0300

    file added 'README.md' and code correction

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -8,65 +8,82 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-int Input(string arg)
+internal class Program
 {
-    int number;
-    System.Console.Write($"Введите {arg}");
-    while(!int.TryParse(Console.ReadLine()!, out number))
+    private static void Main(string[] args)
     {
-        System.Console.WriteLine("Введен не верный размер массива!");
-        System.Console.WriteLine("Размер массив не может равняться нулю.");
-        System.Console.WriteLine("В размере массива не может быть букв.");
-        System.Console.Write("Введите цифру больше ноля: ");
-    }
-    return number;
-}
-int length = Input("длинну массива больше ноля: ");
-string[] array = CreateArray(length);
-
-
-string[] CreateArray(int length)
-{
-    string[] array = new string[length];
-    for (int i = 0; i < array.Length; i++)
-    {
-        System.Console.Write($"Введите {i+1}-й элемент массива: ");
-        array[i] = Console.ReadLine();
-    }
-    return array;
-}
-
-System.Console.WriteLine("\n-----------Ваш массив----------");
-System.Console.Write("[");
-System.Console.Write(string.Join(", ", array));
-System.Console.WriteLine("]");
-
-string[] arrayV1 = new string[array.Length];
-
-string NewArray(string[] array, string[] arrayV1)
-{
-    int n =0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i].Length <= 3)
+        int InputLength(string arg)
         {
-            arrayV1[n] = array[i];
-            n++;
+            int number;
+            Console.Write($"Введите {arg}");
+            while (!int.TryParse(Console.ReadLine()!, out number))
+            {
+                Console.WriteLine("Введен не верный размер массива!");
+                Console.WriteLine("Размер массив не может равняться нулю.");
+                Console.WriteLine("В размере массива не может быть букв.");
+                Console.Write("Введите цифру больше ноля: ");
+            }
+            return number;
         }
-    }
-    return arrayV1[n];
-}
+        int length = InputLength("длинну массива больше ноля: ");
+        string[] array = FillArray(length);
 
-void Print(string[] arrayV1)
-{
-    System.Console.Write("[ ");
-    for (int i = 0; i < arrayV1.Length; i++)
-    {
-        System.Console.Write(arrayV1[i]+ ", ");
-    }
-    System.Console.WriteLine("]");
-}
 
-NewArray(array, arrayV1);
-System.Console.WriteLine("\nНовый массив из строк, длина которых меньше, либо равна 3 символам.");
-Print(arrayV1);
+        string[] FillArray(int length)
+        {
+            string[] array = new string[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите {i + 1}-й элемент массива: ");
+                array[i] = Console.ReadLine();
+            }
+            return array;
+        }
+
+        Console.WriteLine("\n-----------Ваш массив----------");
+        Console.Write("[");
+        Console.Write(string.Join(", ", array));
+        Console.WriteLine("]");
+
+        // string[] arrayV1 = new string[array.Length];
+
+        string[] NewArray(string[] array)
+        {
+            int newLength = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    newLength++;
+                }
+            }
+
+            string[] arrayV1 = new string[newLength];
+
+            int n = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    arrayV1[n] = array[i];
+                    n++;
+                }
+            }
+            return arrayV1;
+        }
+
+        void Print(string[] arrayV1)
+        {
+            Console.Write("[ ");
+            for (int i = 0; i < arrayV1.Length; i++)
+            {
+                Console.Write($"‘{arrayV1[i]}‘ ");
+            }
+            Console.WriteLine("]");
+        }
+
+        NewArray(array);
+        Console.WriteLine("\nНовый массив из строк, длина которых меньше, либо равна 3 символам.");
+        Print(NewArray(array));
+    }
+}
